ccfa30e491b412becfbc738f3e5785d0732ec03d
Alexis Lopez <lopez111alex@gmail.com>
2024-01-17 13:46:05 -0800

    made model for thoughts

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const Reaction = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: [true, "Please add a thought"],
+      minlength: [1, "Your thought must be at least 1 character long"],
+      maxlength: [280, "Your thought must be less than 280 characters"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [Reaction.schema],
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
