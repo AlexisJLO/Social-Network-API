d681309fd6c03984a2c8017b868ba2b3e7e3c662
Alexis Lopez <lopez111alex@gmail.com>
2024-01-17 13:44:53 -0800

    Made index.js for running server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+db.once('open', () => {
+  console.log('Connected to MongoDB database');
+  app.listen(PORT, () => {
+    console.log(`API server running on http://localhost:${PORT}!`);
+  });
+});
